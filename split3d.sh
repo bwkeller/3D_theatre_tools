42ceaca0631b90f03c17cc4780205d6979da8919
Ben Keller <malzraa@gmail.com>
2014-08-21 16:32:04 -0400

    Preserve more entropy by including the green channel

--- split3d.sh
+++ split3d.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 convert $1 -channel R -separate `echo $1 | cut -d'.' -f 1`_l.tga
-convert $1 -channel B -separate `echo $1 | cut -d'.' -f 1`_r.tga
+convert $1 -channel R -evaluate set 0 -colorspace Gray `echo $1 | cut -d'.' -f 1`_r.tga
